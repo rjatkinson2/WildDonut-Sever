9f2ecff5f2b9e56916761e1d79aa4f4c9c9e0808
devmeyster <arthurfbi@yahoo.com>
2015-04-04 15:03:16 -0700

    (feat) Implemented users and bookings routers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var app = require('./server/server.js');
 
 app.listen(app.get('port'));
+
 console.log('Listening on ', app.get('port'));
--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,16 +1,19 @@
 var morgan = require('morgan'),
-		bodyParser = require('body-parser');
+    bodyParser = require('body-parser');
 
 module.exports = function(app, express) {
-	//We can use multiple routers with their own configurations
-	var userRouter = express.Router();
+  //We can use multiple routers with their own configurations
+  var userRouter = express.Router();
+  var bookingsRouter = express.Router();  
+  
+  app.use(morgan('dev'));
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  //add static files  
 
-	app.use(morgan('dev'));
-	app.use(bodyParser.urlencoded({extended: true}));
-	app.use(bodyParser.json());
-	//add static files
-
-	app.use('/api/users', userRouter); // use user router for all user request
-
-	require('../api/users/userRoutes.js')(userRouter);
+  app.use('/api/users', userRouter); // use user router for all user requests
+  app.use('/api/bookings', bookingsRouter); //use bookings router for all booking requests  
+  
+  require('../api/users/userRoutes.js')(userRouter);
+  require('../api/bookings/bookingsRoutes.js')(bookingsRouter);
 };
