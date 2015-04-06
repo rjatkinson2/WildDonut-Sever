b5277a5500d5617608143460ae609e70b73c63cb
Ryan Atkinson <rjatkinson2@gmail.com>
2015-04-06 11:00:57 -0700

    (feat) Implement API route controllers to manage database queries

---
+++ server/api/bookings/bookingController.js
@@ -0,0 +1,13 @@
+var User = require('../../database/models/userModel.js');
+
+module.exports.createBooking = function(req, res, next){
+
+};
+
+module.exports.getUserBookings = function(req, res, next){
+
+};
+
+module.exports.getTeacherBookings = function(req, res, next){
+
+};
