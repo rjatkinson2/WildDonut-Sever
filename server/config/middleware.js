fe8c844d56d40413a7fc838c09e30c6f9f2d3477
devmeyster <arthurfbi@yahoo.com>
2015-04-07 14:52:46 -0700

    Added Cors npm module and custom cross-origin response headers

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,5 +1,6 @@
 var morgan = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    cors = require('cors');
 
 module.exports = function(app, express) {
   //We can use multiple routers with their own configurations
@@ -10,6 +11,12 @@ module.exports = function(app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   //add static files
+  app.use(cors());
+  app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+    next();
+  });
 
   app.use('/api/users', userRouter); // use user router for all user requests
   app.use('/api/bookings', bookingRouter); //use bookings router for all booking requests  
