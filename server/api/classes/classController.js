a971164d57f3dc86465b7863daa8c0b3aae20a62
benignbemine <kyle.17.owen@gmail.com>
2015-04-10 11:56:34 -0700

    updated get all classes to include teacher username

--- server/api/classes/classController.js
+++ server/api/classes/classController.js
@@ -3,7 +3,9 @@ var User = require('../../database/models/userModel.js');
 var Options = require('../../database/models/optionModel.js');
 
 module.exports.getAllClasses = function(req, res, next){
-  Class.find({}, function(err, classInstances){
+  Class.find({})
+       .populate({path: 'teacher', select: 'username'})
+       .exec(function(err, classInstances){
       if(err){
         res.status(400).send('Bad request');
       }else if(!classInstances){
